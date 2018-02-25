81c2a81b1253620945f2906b78d121e62092ca3e
Katsiaryna <36642433+UlakhovichKate@users.noreply.github.com>
2018-02-25 12:46:20 +0300

    result
    result

--- src/index.js
+++ src/index.js
@@ -1,31 +1,56 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        // create empty array.
+        this.array = [];
+        // Sort numbers in an array in ascending order.
+        this.ascendingOrder = function (a, b) {
+            return a - b;
+        }
+    }
 
-  add(element) {
-    // your implementation
-  }
+    // add element and store it in any way inside
+    add(element) {
+        // push elements to created array.
+        this.array.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        // return a specific element from array
+        return this.array[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    //  return the count of current elements
+    get length() {
+        // Return the count of current elements, which were added to Sorter instance via add method.
+        return this.array.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    // return all elements in array
+    toArray() {
+        // Return all elements in array.
+        return this.array;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    // 'sort' - takes indices of already added elements and sorts only these elements.
+    sort(indices) {
+        let tempArray = [];//временная
+        for (let i = 0; i < indices.length; i++) {//пока есть индексы
+            tempArray.push(this.array[indices[i]]);//положить во врем папку индексы
+        }
+        indices.sort(function (a, b) {//сортируем значения с этим индексом 
+            return a - b;
+        });
+        tempArray.sort(this.compar);
+        for (let i = 0; i < indices.length; i++) {
+            this.array[indices[i]] = tempArray[i];
+        }
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    // setComparator - takes compareFunction as parameter and use it while sorting elements.
+    setComparator(compareFunction) {
+        // return this.array.sort(compareFunction);
+        this.compar = compareFunction;
+    }
 }
 
 module.exports = Sorter;
